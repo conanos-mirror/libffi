685e073bc56e5b4ae8bd59581f9ade1f263e89b4
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-11-07 15:37:44 +0800

    fix package name

--- build.py
+++ build.py
@@ -9,8 +9,9 @@ from conanos.sdk.profile import filter
 if platform.system() == 'Windows':
     os.environ['CONAN_VISUAL_VERSIONS']=os.environ.get('CONAN_VISUAL_VERSIONS','15')
 
+PACKAGE_NAME='libffi'	
 if __name__ == "__main__":
     builder = build_template_default.get_builder()
-    filter('bzip2',builder)
+    filter(PACKAGE_NAME,builder)
 
     builder.run()
