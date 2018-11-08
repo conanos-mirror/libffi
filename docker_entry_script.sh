4cff0e727b187bb1a4a6f774d65ff12b9b1a2c0a
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-11-08 16:20:59 +0800

    N.20

--- docker_entry_script.sh
+++ docker_entry_script.sh
@@ -7,6 +7,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get -y install libltdl-dev
 sudo apt-get -y install texinfo  
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y tzdata
 sudo apt-get -y install dejagnu  
  
 ls /usr/share/aclocal/ltdl.m4 -l
