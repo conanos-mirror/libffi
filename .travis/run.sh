b390ab61894d784faca4fb3187263d5985846759
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-11-08 22:38:50 +0800

    .

--- .travis/run.sh
+++ .travis/run.sh
@@ -10,9 +10,4 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-	
-
-
-
-
 python build.py
--- build.py
+++ build.py
@@ -1,43 +1,32 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from bincrafters import build_template_default
+import platform
 import os
 import re
 from cpt.packager import ConanMultiPackager
+from conanos.sdk.profile import filter
 
-__PACKAGE_NAME__ = 'libffi'
+__NAME__ = 'libffi'
 PATTERN_ = re.compile(r'conanio/(?P<compiler>gcc|clang)(?P<version>\d+)(-(?P<arch>\w+))?')
+if platform.system() == 'Windows':
+    os.environ['CONAN_VISUAL_VERSIONS'] = os.environ.get('CONAN_VISUAL_VERSIONS','15')
 
-if __name__ == "__main__":
+os.environ['CONAN_USERNAME'] = os.environ.get('CONAN_USERNAME','conanos')
 
+if __name__ == "__main__":    
+    PATTERN = re.compile(r'conanio/(?P<compiler>gcc|clang)(?P<version>\d+)(-(?P<arch>\w+))?')
+    m = PATTERN.match(os.environ.get('CONAN_DOCKER_IMAGE',''))
     docker_entry_script = ''
-    m = PATTERN_.match(os.environ.get('CONAN_DOCKER_IMAGE',''))
-    if m:
-        docker_entry_script = '/bin/bash docker_entry_script.sh {compiler} {version}'.format(
-            compiler = m.group('compiler'), version = m.group('version') )
+    if m and os.path.exists('docker_entry_script.sh'):
+        compiler = m.group('compiler')
+        version  = m.group('version')
+        arch     = 'x86_64' if not m.group('arch') else m.group('arch')
+        docker_entry_script ='/bin/bash docker_entry_script.sh %s %s %s'%(compiler,version,arch)
+    
         
     builder = ConanMultiPackager(docker_entry_script=docker_entry_script)
     builder.add_common_builds(pure_c=True)
 
-#    filter(__PACKAGE_NAME__,builder)
-    filtered_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        if options["libffi:shared"] == True and settings["arch"] == "x86_64":
-             filtered_builds.append([settings, options, env_vars, build_requires])
-    builder.builds = filtered_builds
+    filter(__NAME__,builder)
     
     builder.run()
-
-
-#from bincrafters import build_template_default
-#import platform
-#import os
-#from conanos.sdk.profile import filter
-#if platform.system() == 'Windows':
-#    os.environ['CONAN_VISUAL_VERSIONS']=os.environ.get('CONAN_VISUAL_VERSIONS','15')
-#
-#PACKAGE_NAME='libffi'	
-#if __name__ == "__main__":
-#    builder = build_template_default.get_builder()
-#    filter(PACKAGE_NAME,builder)
-#
-#    builder.run()
