2f6c90e7fd1f2d855c85a4338ac1c8b82c8c8d78
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-11-08 20:18:29 +0800

    N211

--- docker_entry_script.sh
+++ docker_entry_script.sh
@@ -7,9 +7,6 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get -y install libltdl-dev
 sudo apt-get -y install texinfo  
-export DEBIAN_FRONTEND=noninteractive
-
-
-sudo apt-get install -y apt-utils
+sudo export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y tzdata
 sudo apt-get -y install dejagnu  
