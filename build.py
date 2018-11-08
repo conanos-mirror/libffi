d82c7fe96e82b3ffb0935cf81996f4b4987de848
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-11-08 14:16:29 +0800

    N.12

--- build.py
+++ build.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-
+import re
 from cpt.packager import ConanMultiPackager
 
 __PACKAGE_NAME__ = 'libffi'
+PATTERN_ = re.compile(r'conanio/(?P<compiler>gcc|clang)(?P<version>\d+)(-(?P<arch>\w+))?')
+
 if __name__ == "__main__":
-    command = 'echo start build %s'%__PACKAGE_NAME__
-    #if os.environ.get('CONAN_DOCKER_IMAGE') and os.environ.get('CONAN_GCC_VERSIONS'):
-#    command += ' && sudo chmod 666 /etc/apt/sources.list'
-#    command += ' && sudo echo deb mirrors.kernel.org/ubuntu cosmic main >> /etc/apt/sources.list'
-    command += ' && sudo apt-get update -y' 
-    command += ' && sudo apt-get upgrade -y' 
-    command += ' && sudo apt-get -y install libltdl-dev'
-    command += ' && sudo apt-get -y install texinfo'     
-    command += ' && ls /usr/share/aclocal/ltdl.m4 -l'
-
-    command = 'ls -l && sudo ./docker_entry_script.sh'
-
+#    command = 'echo start build %s'%__PACKAGE_NAME__
+#    #if os.environ.get('CONAN_DOCKER_IMAGE') and os.environ.get('CONAN_GCC_VERSIONS'):
+##    command += ' && sudo chmod 666 /etc/apt/sources.list'
+##    command += ' && sudo echo deb mirrors.kernel.org/ubuntu cosmic main >> /etc/apt/sources.list'
+#    command += ' && sudo apt-get update -y' 
+#    command += ' && sudo apt-get upgrade -y' 
+#    command += ' && sudo apt-get -y install libltdl-dev'
+#    command += ' && sudo apt-get -y install texinfo'     
+#    command += ' && ls /usr/share/aclocal/ltdl.m4 -l'
+#
+#    command = 'ls -l && sudo ./docker_entry_script.sh'
+    docker_entry_script = ''    
+    if PATTERN_.match(os.environ.get('CONAN_DOCKER_IMAGE','')):
+        docker_entry_script = '/bin/sh docker_entry_script.sh'
+        
     builder = ConanMultiPackager(docker_entry_script=command)
     builder.add_common_builds(pure_c=True)
 
