6a72b5112cecbd6364a5285d8e970e60a0826dbd
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-11-08 09:34:36 +0800

    N.1

--- build.py
+++ build.py
@@ -11,11 +11,16 @@ if __name__ == "__main__":
         command = 'sudo apt-get update '
         '&& sudo apt-get install libltdl-dev '
         '&& ls /usr/share/aclocal/ltdl.m4 -l '
-        
+
     builder = ConanMultiPackager(docker_entry_script=command)
     builder.add_common_builds(pure_c=True)
 
-    filter(__PACKAGE_NAME__,builder)
+#    filter(__PACKAGE_NAME__,builder)
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if options["libffi:shared"] == True and settings["arch"] == "x86_64":
+             filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
     
     builder.run()
 
